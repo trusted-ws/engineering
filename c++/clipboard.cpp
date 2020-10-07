4cf33136c90c1f04ca87ad5ca850438673a4c93b
Murilo <eth0.db0@gmail.com>
2020-10-07 03:43:50 -0300

    Add some files

---
+++ c++/clipboard.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <windows.h>
+
+
+int SetClipboardText(const char *text) {
+
+    if(OpenClipboard(nullptr) != 0) {
+
+        HGLOBAL clipbuffer;
+        char *buffer;
+
+        EmptyClipboard();
+
+        clipbuffer = GlobalAlloc(GMEM_DDESHARE, strlen(text) + 1);
+        buffer = (char*) GlobalLock(clipbuffer);
+
+        strcpy(buffer, LPCSTR(text));
+
+        GlobalUnlock(clipbuffer);
+        SetClipboardData(CF_TEXT, clipbuffer);
+
+        CloseClipboard();
+
+        return 0;
+
+    } else {
+        return -1;
+    }
+
+}
+
+std::string GetClipboardText(void) {
+    if(!OpenClipboard(nullptr))
+        return "-error";
+
+    HANDLE hData = GetClipboardData(CF_TEXT);
+
+    if(hData == nullptr) {
+        return "-error";
+    }
+
+    char *pszText = static_cast<char*>(GlobalLock(hData));
+    if(pszText == nullptr)
+        return "-error";
+
+    std::string text(pszText);
+    GlobalUnlock(hData);
+
+    CloseClipboard();
+
+    return text;
+}
+
+int main(void) {
+
+    std::string old_clip = GetClipboardText();
+    std::cout << "Clipboard: " << GetClipboardText() << std::endl;
+
+    /* Se o clipboard atual for 'teste' enttão será substituido por 'teste1234' */
+    if(strcmp(old_clip.c_str(), "teste") == 0) {
+        SetClipboardText("teste1234");
+        std::cout << "New Clipboard: " << GetClipboardText() << std::endl;
+    }
+
+    return 0;
+}
